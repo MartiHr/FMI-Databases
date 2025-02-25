31c707b34b384a6ce397ccedba162f5965083a36
MartiHr <81310884+MartiHr@users.noreply.github.com>
2025-02-25 19:54:05 +0200

    FInished part 2 of Excercise2

--- Exercise2.sql
+++ Exercise2.sql
@@ -116,3 +116,22 @@ SELECT
 FROM pc
 GROUP BY hd
 HAVING COUNT(DISTINCT code) >= 2;
+
+--Problem 6
+SELECT DISTINCT
+    pc1.model,
+    pc2.model 
+FROM 
+    pc AS pc1
+JOIN 
+    pc AS pc2
+ON 
+    pc1.speed = pc2.speed
+    AND pc1.ram = pc2.ram
+    AND pc1.model < pc2.model
+
+--Part 3
+USE ships
+
+--Problem 1
+
