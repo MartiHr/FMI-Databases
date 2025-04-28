d015db6ca6716fad153339f71794d832a16295cf
MartiHr <81310884+MartiHr@users.noreply.github.com>
2025-04-28 12:39:51 +0300

    Finished exercise 8 by completing problem 2

--- Exercise8.sql
+++ Exercise8.sql
@@ -34,3 +34,41 @@ DROP TABLE Product
 DROP TABLE Printer
 
 --Problem 2
+--a)
+CREATE TABLE Users (
+	ID INT PRIMARY KEY,
+	Email VARCHAR(50),
+	[Password] VARCHAR(50),
+	RegisterDate DATETIME2
+)
+
+CREATE TABLE Friends (
+	UID INT,
+	FriendUID INT,
+	PRIMARY KEY(UID, FriendUID),
+	FOREIGN KEY(UID) REFERENCES Users(ID),
+	FOREIGN KEY(FriendUID) REFERENCES Users(ID)
+)
+
+CREATE TABLE Walls(
+	UID INT,
+	MessengerUID INT,
+	Text NVARCHAR(255),
+	Date DATETIME2,
+	FOREIGN KEY(UID) REFERENCES Users(ID),
+	FOREIGN KEY(MessengerUID) REFERENCES Users(ID),
+)
+
+CREATE TABLE Groups(
+	GroupUID INT PRIMARY KEY,
+	Name VARCHAR(50),
+	Description VARCHAR(255) DEFAULT ''
+)
+
+CREATE TABLE GroupMembers (
+    GroupUID INT,
+    UID INT,
+    PRIMARY KEY(GroupUID, UID),
+    FOREIGN KEY(GroupUID) REFERENCES Groups(GroupUID),
+    FOREIGN KEY(uid) REFERENCES Users(id)
+);
